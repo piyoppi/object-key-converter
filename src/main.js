d186d4af39ad0504fc816429f67f4c9d71788470
piyoppi <garakuta.toolbox@gmail.com>
2020-08-13 23:56:47 +0900

    set values when convertedKey isnot found

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ export function convertKey(obj, map, reversed) {
     const convertedKeyIndex = reversed ? 0 : 1;
     const convertedKey = map.find(val => key === val[keyIndex]);
 
-    if( typeof obj[key] === 'object' && convertedKey.length === 3 ) {
-      result[convertedKey[convertedKeyIndex]] = convertKey(obj[key], convertedKey[2], reversed);
-    } else if( convertedKey ) {
-      result[convertedKey[convertedKeyIndex]] = obj[key];
-    } else {
+    if( !convertedKey ) {
       result[key] = obj[key];
+    } else if( typeof obj[key] === 'object' && convertedKey.length === 3 ) {
+      result[convertedKey[convertedKeyIndex]] = convertKey(obj[key], convertedKey[2], reversed);
+    } else {
+      result[convertedKey[convertedKeyIndex]] = obj[key];
     }
   });
 
